ab463b6ea7faafd77810111deec8f23d1da69f42
Nikos Katsos <nikoskeyz@gmail.com>
2017-05-08 21:56:29 +0300

    (refactor) formated code and removed comments

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const FB_CONFIG = {
-    apiKey: "AIzaSyCJKpemXPD7KRVmDebrpHwifNMekijlE_s",
-    authDomain: "todoapp-9774b.firebaseapp.com",
-    databaseURL: "https://todoapp-9774b.firebaseio.com",
-    projectId: "todoapp-9774b",
-    storageBucket: "todoapp-9774b.appspot.com",
-    messagingSenderId: "773465329178"
+  apiKey: "AIzaSyCJKpemXPD7KRVmDebrpHwifNMekijlE_s",
+  authDomain: "todoapp-9774b.firebaseapp.com",
+  databaseURL: "https://todoapp-9774b.firebaseio.com",
+  projectId: "todoapp-9774b",
+  storageBucket: "todoapp-9774b.appspot.com",
+  messagingSenderId: "773465329178"
 };
 
 const ELEM = {
@@ -24,7 +24,7 @@ const ELEM = {
 };
 
 
-(function(){
+(function () {
 
   firebase.initializeApp(FB_CONFIG);
 
@@ -39,47 +39,6 @@ const ELEM = {
   });
 
   initalizeListeners();
-
-/*
-  const txtEmail = document.getElementById('txtEmail');
-  const txtPassword = document.getElementById('txtPassword');
-  const txtFullname = document.getElementById('txtFullname');
-  const txtUsername = document.getElementById('txtUsername');
-  const txtDate = document.getElementById('txtDate');
-  const btnLogin = document.getElementById('btnLogin');
-  const btnSignUp = document.getElementById('btnSignUp');
-  const btnLogout = document.getElementById('btnLogout');
-  const todolidt = document.getElementById('todolidt');
-
-  //Add login event
-
-  btnLogin.addEventListener('click', e => {
-    //email pass
-
-    const email = txtEmail.value;
-    const pass = txtPassword.value;
-    const auth = firebase.auth();
-
-    //sign in
-
-    const promise = ;
-    promise.catch(e => console.log (e.message));
-
-  });
-
-  //add sign up
-
-  btnSignUp.addEventListener('click', e => {
-     //email pass
-     //TODO:check for real email
-    const email = txtEmail.value;
-    const pass = txtPassword.value;
-    const auth = firebase.auth();
-    //sign in
-    const promise = auth.createUserWithEmailAndPassword(email,pass);
-    promise.catch(e => console.log (e.message));
-  });
-*/
 
 }());
 
@@ -100,9 +59,9 @@ function initalizeListeners() {
   });
 
   /* main */
-  ELEM.form.signUp.addEventListener('submit', function(event) {
+  ELEM.form.signUp.addEventListener('submit', function (event) {
     event.preventDefault();
-    
+
     data = serializeForm(this);
     firebase.auth().createUserWithEmailAndPassword(data.email, data.password)
       .then(response => {
@@ -113,9 +72,9 @@ function initalizeListeners() {
       })
   });
 
-  ELEM.form.signIn.addEventListener('submit', function(event) {
+  ELEM.form.signIn.addEventListener('submit', function (event) {
     event.preventDefault();
-    
+
     data = serializeForm(this);
     firebase.auth().signInWithEmailAndPassword(data.email, data.password)
       .then(response => {
@@ -133,7 +92,7 @@ function serializeForm(formElement) {
   const response = {};
 
   for (const input of inputs) {
-    Object.assign(response, {[input.name] : input.value});
+    Object.assign(response, { [input.name]: input.value });
   }
 
   return response;
